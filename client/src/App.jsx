89a7cb8644a060e4ff06ac4f24d0b7eff34fc28c
AdamSkubida <adasiow.s@gmail.com>
2023-12-03 20:24:56 +0100

    featire/balance_adam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { TestStoreReduxComponent } from "./components/TestReduxStore";
+import { Balance } from "./components/Balance/Balance";
 
 function App() {
   return (
     <>
       <TestStoreReduxComponent />
+      <Balance />
     </>
   );
 }
