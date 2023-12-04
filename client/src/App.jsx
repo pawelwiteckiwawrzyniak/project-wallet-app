c9c57de10520f89f389d98bc718383e4fc040a2b
Evgenij Prishchepov <prishchepovee@gmail.com>
2023-12-05 01:38:52 +0300

    add protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,50 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { TestStoreReduxComponent } from "./components/TestReduxStore";
 import { Balance } from "./components/Balance/Balance";
 import { ChartWrapper } from "./components/Chart/ChartWrapper";
+import { useDispatch } from "react-redux";
+import { useAuth } from "./hooks/userAuth";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
+import { useEffect } from "react";
+import { refreshUserTest } from "./redux/auth/operations";
 
 function App() {
+  const dispatch = useDispatch();
+  const { isRefresh } = useAuth();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        //use test function
+        await dispatch(refreshUserTest());
+      } catch (error) {
+        console.error("Error refreshing user:", error);
+      }
+    };
+    fetchData();
+  }, [dispatch]);
+
   return (
     <>
-      <TestStoreReduxComponent />
-      <Balance />
-      <ChartWrapper />
+      <Routes>
+        {isRefresh ? (
+          //there should be a loader
+          <Route />
+        ) : (
+          <Route path="/" element={<ProtectedRoute />}>
+            {/* Add components below, which would be display for logged-in user.*/}
+            <Route
+              path="/"
+              element={
+                <div>
+                  <Balance />
+                  <ChartWrapper />
+                </div>
+              }
+            />
+          </Route>
+        )}
+      </Routes>
     </>
   );
 }
