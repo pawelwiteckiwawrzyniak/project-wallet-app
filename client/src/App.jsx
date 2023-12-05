83886a8a6cfc5760611abbe744db47f8c5adc986
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-12-05 21:21:23 +0100

    register/login form button bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,30 @@ import "./App.css";
 import { TestStoreReduxComponent } from "./components/TestReduxStore";
 import { Balance } from "./components/Balance/Balance";
 import LoginForm from "./components/LoginForm/LoginForm";
+import SignupForm from "./components/RegistrationForm/RegistrationForm";
 import { ChartWrapper } from "./components/Chart/ChartWrapper";
+import React, { useState } from "react";
 
 function App() {
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const handleLoginClick = () => {
+    setShowLoginForm(true);
+  };
+
+  const handleRegisterClick = () => {
+    setShowLoginForm(false);
+  };
+
   return (
     <>
       <TestStoreReduxComponent />
       <Balance />
-      <LoginForm />
+      {showLoginForm ? (
+        <LoginForm onRegisterClick={handleRegisterClick} />
+      ) : (
+        <SignupForm onLoginClick={handleLoginClick} />
+      )}
       <ChartWrapper />
     </>
   );
