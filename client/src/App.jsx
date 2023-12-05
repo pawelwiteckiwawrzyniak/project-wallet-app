79e3a4d1ca6683ebd415afca8bed7573a49ca761
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-12-05 17:51:16 +0100

    LoadSpinner/ModalLoadOut
    wczoraj wysłałem, ale coś nie poszło, wysyłam jeszcze raz i tak trochę poprawione

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,49 @@
-import "./App.css";
-import { TestStoreReduxComponent } from "./components/TestReduxStore";
-import { Balance } from "./components/Balance/Balance";
 import LoginForm from "./components/LoginForm/LoginForm";
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { TestStoreReduxComponent } from './components/TestReduxStore';
+import { Balance } from './components/Balance/Balance';
+import {ModalLoadOut} from './components/ModalLoadOut/ModalLoadOut';
+import {LoadSpinner} from './components/LoadSpinner/LoadSpinner';
 import { ChartWrapper } from "./components/Chart/ChartWrapper";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const isLoading = useSelector((state) => state.global.isLoading);
+  const dispatch = useDispatch();
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    dispatch({ type: 'START_LOADING' });
+
+    setTimeout(() => {
+      dispatch({ type: 'STOP_LOADING' });
+    }, 3000);
+  }, [dispatch]);
   return (
     <>
       <TestStoreReduxComponent />
+      <ToastContainer />
       <Balance />
       <LoginForm />
       <ChartWrapper />
+      <button className="exit-button" onClick={handleOpenModal}>
+        EXIT
+      </button>
+      {isModalOpen && <ModalLoadOut onClose={handleCloseModal} />}
+      <LoadSpinner loading={isLoading} />
     </>
-  );
+  )
 }
-
 export default App;
---
+++ client/src/components/ModalLoadOut/ModalLoadOut.jsx
@@ -0,0 +1,80 @@
+
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logOut } from '../../redux/auth/operations';
+import { globalSlice } from '../../redux/store';
+import { toast } from 'react-toastify';
+import { LoadSpinner } from '../LoadSpinner/LoadSpinner';
+import css from './ModalLoadOut.module.css';
+
+export const ModalLoadOut = ({ onClose }) => {
+  const dispatch = useDispatch();
+  const [isLogoutLoading, setIsLogoutLoading] = useState(false);
+  const isLoading = useSelector((state) => state.global.isLoading);
+
+  const handleOverlayClick = (event) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleEscape = (event) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscape);
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [onClose]);
+
+  const handleLogout = async () => {
+    try {
+      dispatch(globalSlice.actions.setLoading(true)); 
+      setIsLogoutLoading(true);
+
+      const resultAction = await dispatch(logOut());
+
+      if (logOut.rejected.match(resultAction)) {
+        console.error('Logout failed. Please try again.', resultAction.payload);
+        toast.error('Logout failed. Please try again.');
+      } else {
+        toast.success('Logout successful');
+        onClose();
+      }
+    } catch (error) {
+      console.error('An error occurred during logout.', error);
+      toast.error('An error occurred during logout. Please try again.');
+    } finally {
+      dispatch(globalSlice.actions.setLoading(false)); 
+      setIsLogoutLoading(false);
+    }
+  };
+
+  const handleNoClick = () => {
+    onClose();
+  };
+
+  return (
+    <div className={css.overlay} onClick={handleOverlayClick}>
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+        <div className={css.header}>Load Out</div>
+        <div className={css.content}>
+          <p>Do you want to log out?</p>
+        </div>
+        <div className={css.buttonContainer}>
+          <button className={css.noButton} onClick={handleNoClick}>
+            No
+          </button>
+          <button className={css.yesButton} onClick={handleLogout}>
+            Yes
+          </button>
+        </div>
+        <LoadSpinner loading={isLoading || isLogoutLoading} />
+      </div>
+    </div>
+  );
+};
