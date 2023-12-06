6f0f2f7364d5e24f1c7722be9485fd28ed32546f
Filip Vukić <filipvukic90@gmail.com>
2023-12-06 22:23:37 +0100

    editTransaction
    - added editTransactionModal
    - created style for editTransaction
    - added missing selector

--- client/src/redux/transactions/selectors.js
+++ client/src/redux/transactions/selectors.js
@@ -3,3 +3,4 @@ export const selectIsLoading = (state) => state.transactions.isLoading;
 export const selectError = (state) => state.transactions.error;
 export const selectTransactionsCategories = (state) =>
   state.transactions.categories;
+export const selectTransactionId = (state) => state.transactions.selectedId;
