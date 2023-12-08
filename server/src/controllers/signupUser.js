c92ec8c79b042da412990e5e6b5b0975c132f652
Paweł Witecki-Wawrzyniak <pawel.witecki.wawrzyniak@gmail.com>
2023-12-08 01:03:13 +0100

    Update signupUser.js

--- server/src/controllers/signupUser.js
+++ server/src/controllers/signupUser.js
@@ -1,3 +1,4 @@
+import { genToken } from "../middleware/authToken.js";
 import { User } from "../models/users.js";
 
 export async function signupUser(req, res, next) {
@@ -8,12 +9,21 @@ export async function signupUser(req, res, next) {
       .status(409)
       .json({ description: "User with such email already exists" });
   try {
-    const newUser = await User.create({ email, password, name });
+    const newUser = await User.create({ email, password, name, token });
+    const userPayload = {
+      id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      balance: newUser.balance,
+    };
+    const token = genToken(userPayload);
+    await User.findOneAndUpdate({ email: newUser.email }, { token });
     return res.status(201).json({
       user: {
         name: newUser.name,
         email: newUser.email,
         balance: newUser.balance,
+        token: newUser.token,
       },
     });
   } catch (error) {
