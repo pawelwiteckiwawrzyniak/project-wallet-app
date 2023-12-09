1526f017f560ffa59bacd2e1754544e95f942f7f
Paweł Witecki-Wawrzyniak <pawel.witecki.wawrzyniak@gmail.com>
2023-12-09 23:19:41 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import LoginForm from "./components/LoginForm/LoginForm";
-import SignupForm from "./components/RegistrationForm/RegistrationForm";
+/* import LoginForm from "./components/LoginForm/LoginForm"; */
+/* import SignupForm from "./components/RegistrationForm/RegistrationForm"; */
 import { useState, useEffect } from "react";
-import { useDispatch /* , useSelector  */ } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Balance } from "./components/Balance/Balance";
 /* import { ModalLoadOut } from "./components/ModalLoadOut/ModalLoadOut"; */
 /* import { LoadSpinner } from "./components/LoadSpinner/LoadSpinner"; */
-import { ChartWrapper } from "./components/Chart/ChartWrapper";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+/* import { ChartWrapper } from "./components/Chart/ChartWrapper"; */
+/* import { ToastContainer } from "react-toastify"; */
+/* import "react-toastify/dist/ReactToastify.css"; */
 import "./App.css";
 import { useAuth } from "./hooks/userAuth";
 import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
@@ -16,13 +16,13 @@ import { refreshUser } from "./redux/auth/operations";
 /* import { TestRegistrationForm } from "./components/TestForms/TestRegistrationForm"; */
 /* import { TestLoginForm } from "./components/TestForms/TestLoginForm"; */
 /* import { refreshUserTest } from "./redux/auth/operations"; */
-import { Currency } from "./components/Currency/Currency";
-
+/* import { Currency } from "./components/Currency/Currency";
+ */
 function App() {
   const dispatch = useDispatch();
   const { isRefresh, isAuth } = useAuth();
-  /* const [isModalOpen, setIsModalOpen] = useState(false); */
-  /* const isLoading = useSelector((state) => state.global.isLoading); */
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const isLoading = useSelector((state) => state.global.isLoading);
 
   const [showLoginForm, setShowLoginForm] = useState(true);
 
@@ -34,13 +34,13 @@ function App() {
     setShowLoginForm(false);
   };
 
-  /* const handleOpenModal = () => {
+  const handleOpenModal = () => {
     setIsModalOpen(true);
-  }; */
+  };
 
-  /*  const handleCloseModal = () => {
+  const handleCloseModal = () => {
     setIsModalOpen(false);
-  }; */
+  };
 
   useEffect(() => {
     dispatch({ type: "START_LOADING" });
@@ -57,33 +57,20 @@ function App() {
   return (
     <>
       <Routes>
-        {isRefresh ? (
-          //there should be a loader
-          <Route />
-        ) : isAuth ? (
-          <Route path="/" element={<ProtectedRoute />}>
-            {/* Add components below, which would be display for logged-in user.*/}
-            <Route
-              path="/"
-              element={
-                <div>
-                  <Balance />
-                  <Currency />
-                  <ChartWrapper />
-                </div>
-              }
-            />
-          </Route>
-        ) : (
-          <Route path="/" element={<div>No access. Register or login. </div>} />
-        )}
+        <Route path="/">
+          {/* Add components below, which would be display for logged-in user.*/}
+          <Route
+            path="/"
+            element={
+              <div>
+                <Balance />
+                {/* <Currency />
+                  <ChartWrapper /> */}
+              </div>
+            }
+          />
+        </Route>
       </Routes>
-      <ToastContainer />
-      {showLoginForm ? (
-        <LoginForm onRegisterClick={handleRegisterClick} />
-      ) : (
-        <SignupForm onLoginClick={handleLoginClick} />
-      )}
     </>
   );
 }
